96e70415d223a7d42fae765389e407bfe061eb5f
HG300404 <nglienhg000@gmail.com>
2024-09-25 01:58:18 +0700

    notification

--- components/CallNotification.tsx
+++ components/CallNotification.tsx
@@ -2,35 +2,38 @@
 import { useSocket } from '@/context/SocketContext'
 import React from 'react'
 import Avatar from "./Avatar";
-import {MdCall, MdCallEnd} from "react-icons/md";
+import { MdCall, MdCallEnd } from "react-icons/md";
 
 const CallNotification = () => {
-const {ongoingCall, handleJoinCall, handleHangup} = useSocket();
+    const { ongoingCall, handleJoinCall, handleHangup } = useSocket();
 
     if (!ongoingCall?.isRinging) return;
-  return (
-    <div className='absolute bg-slate-500 bg-opacityh-70 w-screen h-screen top-0 left-0 flex item-center justify-center'>
-        <div className="bg-white min-w-[300px] min-h-[100px] flex flex-col items-center justify-center rounded p-4">
-            <div className="flex flex-col items-center">
-                <Avatar src={ongoingCall.participants.caller.profile.imageUrl}/>
-                <h3>{ongoingCall.participants.caller.profile.fullName?.split(' ')[0]}</h3>
-            </div>
-            <p className="text-sm mb-2">
-                Incoming Call
-            </p>
-            <div>
-                <button onClick={() => handleJoinCall(ongoingCall)}
-                    className="w-10 h-10 bg-green-500 rounded-full flex items-center justify-center text-white"><MdCall
-                    size={24} /></button>
-                <button onClick={()  => handleHangup(
-                { ongoingCall: ongoingCall ? ongoingCall : undefined, isEmitHangup: true})}
-                    className="w-10 h-10 bg-rose-500 rounded-full flex items-center justify-center text-white">< MdCallEnd
-                    size={24}/> </button>
+    return (
+        <div className='absolute bg-slate-500 bg-opacityh-70 w-screen h-screen top-0 left-0 flex item-center justify-center'>
+            <div className="bg-white min-w-[300px] min-h-[100px] flex flex-col items-center justify-center rounded p-4">
+                <div className="flex flex-col items-center">
+                    <Avatar src={ongoingCall.participants.caller.profile.imageUrl} />
+                    <h3>{ongoingCall.participants.caller.profile.fullName?.split(' ')[0]}</h3>
+                </div>
+                <p className="text-sm mb-2">
+                    Incoming Call
+                </p>
+                <div>
+                    <button onClick={() => handleJoinCall(ongoingCall)}
+                        className="w-10 h-10 bg-green-500 rounded-full flex items-center justify-center text-white"><MdCall
+                            size={24} /></button>
+                    <button onClick={() => {
+                        if (ongoingCall) {
+                            handleHangup({ ongoingCall, isEmitHangup: true });
+                        }
+                    }} className="w-10 h-10 bg-rose-500 rounded-full flex items-center justify-center text-white">
+                        <MdCallEnd size={24} />
+                    </button>
 
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default CallNotification
